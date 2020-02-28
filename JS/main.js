e95af4d2514b737bc2911bb459bc63a3f5894c17
David Thiman <David.thiman@gmail.com>
2020-02-28 08:05:31 +0100

    Basic Javascript

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,55 @@
-var counter = 0;
+// // var apparently not in use, let, const
+// // https://www.youtube.com/watch?v=hdI2bqOjy3c&t=157s
+// // Data Types //
+// // String, Numbers, Boolean, Null, Undefined, Symbol
+
+// const constants = 'Tech, Comp, It, Code';
+// const name = 'David';
+// const age = 26;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined; //Explicitly defined undefined
+// let z;
+
+// console.log(typeof x);
+// // Concatenation
+// console.log('mu name is ' + name + ' and I am ' + age + ' years old!');
+// //Template String
+// console.log(`my name is ${name} and fuck off`);
+
+// console.log(name.length);
+// console.log(constants.split(', '));
+
+// //Arrays - Multiple hold variables
+
+// const numbers = new Array(1,2,3,4,5,6,7,8,9,0);
+
+// const fruits = ['apple', 'oranges', 'banana', 10, true];
+// console.log(numbers);
+// console.log(fruits);
+// fruits[3] = 'greenbeans';
+// console.log(fruits[0], fruits[2]);
+
+// fruits.unshift('Strawberry');
+// fruits.push('mangos');
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+// Object variables
+
+const person = {
+    firstname: 'David',
+    lastName: 'Thiman',
+    age: 26,
+    hobbies: ['programming', 'learning', 'outside activites'],
+    address: {
+        street: 'rymdgatan',
+        city: 'borlänge',
+        state: 'Dalarna'
+    }
+}
+
+console.log(person.age, person.lastName, person.address.state);
